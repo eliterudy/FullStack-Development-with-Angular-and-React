607a5310262308fcac5bef907b8d1ffc0f59d2b3
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-05-23 17:13:56 -0400

    created Menu Component

---
+++ Front-End  Web Development with React/Exercises/confusion/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color={"primary"}>
+        <div className="container">
+          <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
