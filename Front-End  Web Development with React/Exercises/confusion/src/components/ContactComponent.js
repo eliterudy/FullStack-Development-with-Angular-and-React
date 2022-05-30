75042db6b8226b0431aeaf49c24266aec367d73c
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-05-30 19:36:32 -0400

    react react-redux-form

--- Front-End  Web Development with React/Exercises/confusion/src/components/ContactComponent.js
+++ Front-End  Web Development with React/Exercises/confusion/src/components/ContactComponent.js
@@ -13,6 +13,13 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
+const required = (value) => value && value.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 const Contact = (props) => {
   const [errorMessages, updateErrorMessage] = useState({
     firstname: "",
@@ -101,6 +108,21 @@ const Contact = (props) => {
                   className="form-control"
                   name="firstname"
                   placeholder="First Name"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".firstname"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
                 />
               </Col>
             </Row>
@@ -116,6 +138,21 @@ const Contact = (props) => {
                   name="lastname"
                   className="form-control"
                   placeholder="Last Name"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".lastname"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
                 />
               </Col>
             </Row>
@@ -131,6 +168,23 @@ const Contact = (props) => {
                   name="telnum"
                   className="form-control"
                   placeholder="Tel. Number"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                    isNumber,
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".telnum"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 numbers",
+                    maxLength: "Must be 15 numbers or less",
+                    isNumber: "Must be a number",
+                  }}
                 />
               </Col>
             </Row>
@@ -146,6 +200,19 @@ const Contact = (props) => {
                   name="email"
                   className="form-control"
                   placeholder="Email"
+                  validators={{
+                    required,
+                    validEmail,
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".email"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    validEmail: "Must be a valid email",
+                  }}
                 />
               </Col>
             </Row>
