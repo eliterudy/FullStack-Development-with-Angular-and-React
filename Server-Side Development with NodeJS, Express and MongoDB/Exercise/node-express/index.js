958efffa4c45b68a77f7fedf024a24fe16738504
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-06-12 19:12:29 -0400

    express serve static files

--- Server-Side Development with NodeJS, Express and MongoDB/Exercise/node-express/index.js
+++ Server-Side Development with NodeJS, Express and MongoDB/Exercise/node-express/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = "3000";
 
 const app = express();
+// morgan is used to generate logs
+app.use(morgan("dev"));
 
+// provides static files to be accessible via port/filename.extension
+app.use(express.static(__dirname + "/public"));
+
+// default execution
 app.use((req, res, next) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body>This is an Express Server</body></html");
