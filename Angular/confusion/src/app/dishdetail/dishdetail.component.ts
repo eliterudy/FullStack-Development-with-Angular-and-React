c8bbd148af9b2d1326105ade59362dffcb6db35b
Gavin Dmello <gavinrocks1997@gmail.com>
2023-07-04 19:24:04 -0400

    Angular - RxJS Part 2: New approach to get Id from params and get dish details | using subscriber "params" from this.router and using pipe of switchMap to read the new latest params and make api call.

--- Angular/confusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/confusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs/operators';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -11,6 +12,9 @@ import { Location } from '@angular/common';
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish | undefined;
+  dishIds: string[] | undefined;
+  prev: string | undefined;
+  next: string | undefined;
 
   constructor(
     private dishservice: DishService,
@@ -18,13 +22,39 @@ export class DishdetailComponent implements OnInit {
     private location: Location
   ) {}
 
-  ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe((dish) => {
-      this.dish = dish;
-    });
+  async ngOnInit() {
+    // Older Approach: Using Snapshot | This needs page refresh
+    // const id = this.route.snapshot.params['id'];
+    // this.dishservice.getDish(id).subscribe((dish) => {
+    //   this.dish = dish;
+    // });
+
+    // Newer Approach: Using Subscriber to Observable | This DOESN'T needs page refresh
+
+    await this.dishservice
+      .getDishIds()
+      .subscribe((dishIds) => (this.dishIds = dishIds));
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => this.dishservice.getDish(params['id']))
+      )
+      .subscribe((dish) => {
+        this.dish = dish;
+        this.setPrevNext(dish.id);
+      });
   }
 
+  setPrevNext(dishId: string) {
+    if (this.dishIds) {
+      const index = this.dishIds.indexOf(dishId);
+      this.prev =
+        this.dishIds![
+          (this.dishIds!.length + index - 1) % this.dishIds!.length
+        ];
+      this.next =
+        this.dishIds[(this.dishIds.length + index + 1) % this.dishIds?.length];
+    }
+  }
   goBack(): void {
     this.location.back();
   }
