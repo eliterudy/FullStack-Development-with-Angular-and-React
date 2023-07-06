93d21f97345896c79efc49d0fd5697d8e6e3a6a1
Gavin Dmello <gavinrocks1997@gmail.com>
2023-07-06 01:07:28 -0400

    Angular - Submission Week 3

--- Angular/confusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/confusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import messages from '../shared/messages';
+import { Comment } from '../shared/comment';
+const { formErrorMessages } = messages || {};
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
+  // Update form in template for comment form
+  @ViewChild('fform')
+  commentFormDirective: any;
+
   dish: Dish | undefined;
   dishIds: string[] | undefined;
   prev: string | undefined;
   next: string | undefined;
 
+  // Comment Form Group
+  commentForm!: FormGroup;
+  comment!: Comment;
+
+  // Comment Form Error messages
+  commentFormError: any = {
+    author: '',
+    comment: '',
+  };
+
   constructor(
     private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location
-  ) {}
+    private location: Location,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
 
   async ngOnInit() {
     // Older Approach: Using Snapshot | This needs page refresh
@@ -44,6 +66,34 @@ export class DishdetailComponent implements OnInit {
       });
   }
 
+  // create a new form for accepting user comments
+  createForm = () => {
+    this.commentForm = this.fb.group({
+      author: [
+        '',
+        [
+          Validators.required, // validation to check required
+          Validators.minLength(2), // validation to check min length
+        ],
+      ],
+      rating: 4,
+      comment: [
+        '',
+        [
+          Validators.required, // validation to check required
+          Validators.minLength(1), // validation to check min length
+          Validators.maxLength(100),
+        ], // validation to check max length
+      ],
+    });
+
+    this.commentForm.valueChanges.subscribe((data) =>
+      this.onValueChanged(data)
+    );
+
+    this.onValueChanged(); // (re)set validation messages now
+  };
+
   setPrevNext(dishId: string) {
     if (this.dishIds) {
       const index = this.dishIds.indexOf(dishId);
@@ -55,7 +105,46 @@ export class DishdetailComponent implements OnInit {
         this.dishIds[(this.dishIds.length + index + 1) % this.dishIds?.length];
     }
   }
+
   goBack(): void {
     this.location.back();
   }
+
+  onValueChanged(data?: any) {
+    console.log(this.commentForm.status == 'VALID');
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.commentFormError) {
+      if (this.commentFormError.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.commentFormError[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = formErrorMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.commentFormError[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // on submit for new comment form
+  onSubmit = () => {
+    this.dish?.comments.push({
+      ...this.commentForm.value,
+      date: new Date().toISOString(),
+    });
+    console.log(this.comment);
+    this.commentForm.reset({
+      author: '',
+      rating: 1,
+      comment: '',
+    });
+    this.commentFormDirective.resetForm();
+  };
 }
